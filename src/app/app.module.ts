505f4ce60c47cfdf478f2888422a992b7e195eb6
HenriqueT <ph564131@gmail.com>
2023-10-03 04:06:53 -0300

    adding the draw of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { SmallCardsComponent } from './components/small-cards/small-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SmallCardsComponent
   ],
   imports: [
     BrowserModule,
